5a28932c1fde6281f202565f9c93215a213e5410
Muhammad Waqas <mw23acy@herts.ac.uk>
2024-11-17 13:15:28 +0000

    mean identified and commands

--- mean identified.R
+++ mean identified.R
@@ -1,2 +1,10 @@
 library(readr)
 View(df)
+datafile <- read_csv("5- respiratory-infection-death-rate-who-mdb.csv")
+df<-datafile
+names(df)[1] <- "Entity"
+names(df)[1] <- "Country"
+names(df)[2] <- "Code"
+names(df)[3] <- "year"
+names(df)[4] <- "Death"
+mean(df$Death)
