0183d3dd7572f2140b85eeb37b13c3fb9fae723d
Muhammad Waqas <mw23acy@herts.ac.uk>
2024-11-18 09:29:19 +0000

    wilcox text result is also add

--- t test file.R
+++ t test file.R
@@ -1,11 +1,14 @@
 library(dplyr)
-# Filter data for the years 2020 and 2021
-df_subset <- df2 %>% filter(year == 2020 | year == 2021)
 # Filter data for the years 2020 and 2021 without using %>%
-df_subset <- filter(df2, year == 2020 | year == 2021)
+df2_subset <- filter(df, year == 1950 | year == 2021)
 # Base R alternative without using dplyr
-df_subset <- df2[df2$year == 2020 | df2$year == 2021, ]
+df_subset <- df2_subset[df2_subset$year == 1950 | df2_subset$year == 2021, ]
 # Perform the t-test on Death count for these two years
 t_test_result <- t.test(df_subset$Death ~ df_subset$year)
 # Print the p-value and test results
 print(t_test_result)
+
+
+
+wilcox.test(df_subset$Death ~ df_subset$year) # for non parametric data
+          
