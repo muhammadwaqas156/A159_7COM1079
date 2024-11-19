e105f567e37b4c1473759725f4dad2363b7ec122
Muhammad Waqas <mw23acy@herts.ac.uk>
2024-11-19 20:09:43 +0000

    T Test of top two countries

---
+++ t_test of the top two country.R
@@ -0,0 +1,22 @@
+library(dplyr)
+
+# Calculate mean death rate by country
+country_means <- df %>%
+  group_by(Country) %>%
+  summarize(mean_death_rate = mean(Death, na.rm = TRUE)) %>%
+  arrange(desc(mean_death_rate))
+
+head(df)
+
+# Select top 2 countries
+top_2_countries <- head(country_means, 2)$Country
+
+# Filter for top 2 countries
+top_2_data <- df %>% filter(Country %in% top_2_countries)
+
+# Verify data
+print(unique(top_2_data$Country))
+
+# Perform t-test
+t_test_result <- t.test(Death ~ Country, data = top_2_data)
+print(t_test_result)
