5a2bf2197e16d569827ed062471fef1a585e273c
Muhammad Waqas <mw23acy@herts.ac.uk>
2024-11-20 15:33:05 +0000

    commiting data in top ten countries

--- boxplot for two countries on the basis of mean.R
+++ boxplot for two countries on the basis of mean.R
@@ -2,19 +2,22 @@ library(dplyr)
 library(ggplot2)
 library(forcats)
 
+
 # Calculate mean death rate for each country
 top_2_countries <- data %>%
   group_by(Entity) %>%
   summarise(Mean_Death_Rate = mean(Death_Rate, na.rm = TRUE)) %>%
   arrange(desc(Mean_Death_Rate)) %>%
-  slice(1:2)  # Select top 2 countries
+  slice(1:10)  # Select top 2 countries
+
+head(top_2_countries)
 
 # Filter the dataset for only the top 2 countries
 filtered_data <- data %>%
   filter(Entity %in% top_2_countries$Entity)
 
 # Plot boxplot for the top 2 countries
-ggplot(filtered_data, aes(x = fct_reorder(Entity, Death_Rate, .fun = median, na.rm = TRUE), y = Death_Rate)) +
+ggplot(filtered_data, aes(x = fct_reorder(Entity, Death_Rate, .fun = mean, na.rm = TRUE), y = Death_Rate)) +
   geom_boxplot(fill = "skyblue", color = "black", outlier.color = "red", outlier.shape = 16) +
   stat_summary(fun = mean, geom = "point", shape = 18, color = "darkorange", size = 3) +  # Add mean as a point
   labs(title = "Boxplot of Death Rates for Top 2 Countries by Mean",
