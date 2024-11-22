aa1c653cb901287ede0411ed343901340cffad5e
Muhammad Waqas <mw23acy@herts.ac.uk>
2024-11-22 10:50:36 +0000

    wicox test

---
+++ wilcox test.R
@@ -0,0 +1,23 @@
+library(dplyr)
+
+# Calculate mean death rate by country
+country_means <- df %>%
+  group_by(Country) %>%
+  summarize(mean_death_rate = mean(Death, na.rm = TRUE)) %>%
+  arrange(desc(mean_death_rate))
+
+head(df)
+
+# Select top 2 countries
+top_2_countries <- head(country_means, 2)$Country
+
+# Filter for top 2 countries
+top_2_data <- df %>% filter(Country %in% top_2_countries)
+
+# Verify data
+print(unique(top_2_data$Country))
+
+# Perform wilcox test
+mann_whitney_result <- wilcox.test(Death ~ Country, data = top_2_data)
+print(mann_whitney_result)
+
